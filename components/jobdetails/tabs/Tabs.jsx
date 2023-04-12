87ee20d6beb299e9752245228ca459fa7660406e
Aman Deshpande <amandeshpande392@gmail.com>
2023-04-12 15:24:09 +0530

    initial commit

---
+++ components/jobdetails/tabs/Tabs.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { TouchableOpacity, FlatList, Text, View } from "react-native";
+import TabButton from "./TabButton";
+
+import styles from "./tabs.style";
+import { SIZES } from "../../../constants";
+
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+        keyExtractor={(item) => item}
+      />
+    </View>
+  );
+};
+
+export default Tabs;
