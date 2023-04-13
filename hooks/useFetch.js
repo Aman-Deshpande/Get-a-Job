f1624f88d2cd14debfd58d92a3253476156a95fa
Aman Deshpande <amandeshpande392@gmail.com>
2023-04-13 11:41:14 +0530

    initial commit

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '402beb2dccmshbff14626f31c669p1e6fb9jsnd38ea6d5ea66',
+      "X-RapidAPI-Key": '',
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
